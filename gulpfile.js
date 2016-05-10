0bde26f8cbb48590bfd2d25720160d0e2ea35a59
checkkdahustle <checkkdahustle@fullsail.edu>
2016-05-09 23:44:50 -0400

    add gulp and gulp-nodemon to a gulp task and added it to the de dependecies.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+var nodemon = require('gulp-nodemon');
+
+gulp.task('server', function () {
+	nodemon({
+		script: "server.js",
+		ext: "js",
+		ignore: ["gulp*", "public*"]
+	})
+})
+
+gulp.task('default', ['server'], function () {
+		console.log('Server is ON');
+})
