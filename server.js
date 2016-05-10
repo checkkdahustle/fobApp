8a5ebd08b56676940073d73a0eb52ac69fd8f264
checkkdahustle <checkkdahustle@fullsail.edu>
2016-05-10 00:06:49 -0400

    change root path from index.html to the public folder.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(''));
+app.use(express.static('public'));
 var server = app.listen(process.env.PORT || 5050, function () {
 	console.log('Server listening on PORT:', server.address().port);
 })
