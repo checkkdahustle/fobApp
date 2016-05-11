019b5935ecacce3306ea7714e1672f0e0f2a8569
checkkdahustle <checkkdahustle@fullsail.edu>
2016-05-11 19:33:46 -0400

    add routes to the controller app and add ng view to index

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+// dependencies for the modules will go inside of the array for it to work.
+var fobApp = angular.module('fobApp' , ['ngRoute']);
+
+// Create an Controller, dependencies is inside of the arrary.nremember, Scope give us access to the view or template.
+fobApp.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider.
+		when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'RegistrationController'
+		}).
+		when('/register', {
+			templateUrl: 'views/register.html',
+			controller: 'RegistrationController'
+		}).
+		when('/sccuess', {
+			templateUrl: 'views/sccuess.html',
+			controller: 'SccuessController'
+		}).
+		otherwise({
+			redirectTo: 'login'
+		});
+
+}]);
