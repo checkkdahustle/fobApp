193c42823babbdd2776b5dcabd8b34c79962a3ee
checkkdahustle <checkkdahustle@fullsail.edu>
2016-05-12 18:00:35 -0400

    adjusted lines of code to be more readable.

--- public/js/controllers/registration.js
+++ public/js/controllers/registration.js
@@ -1,14 +1,11 @@
-fobApp.controller('RegistrationController', [
-	'$scope',
-	'Authentication',
-	function ($scope, Authentication) {
+fobApp.controller('RegistrationController', ['$scope', 'Authentication', function ($scope, Authentication) {
 
-		$scope.login = function(){
-			Authentication.login($scope.user);
-		}; // close Login from Authentication
+	$scope.login = function(){
+		Authentication.login($scope.user);
+	}; // close Login from Authentication
 
-		$scope.register = function(){
-			Authentication.register($scope.user);
-		}; // close register from Authentication
+	$scope.register = function(){
+		Authentication.register($scope.user);
+	}; // close register from Authentication
 
-	}]); // close Registration Controller function.
+}]); // close Registration Controller function.
