26f52975f5e384deaf5f3d1a0c60c02455f6b385
checkkdahustle <checkkdahustle@fullsail.edu>
2016-05-12 17:56:36 -0400

    changed input in auth registration function to define registers name.

--- public/js/controllers/registration.js
+++ public/js/controllers/registration.js
@@ -1,26 +1,14 @@
 fobApp.controller('RegistrationController', [
 	'$scope',
-	'$firebaseAuth',
-	'FIREBASE_URL', function ($scope, $firebaseAuth, FIREBASE_URL) {
+	'Authentication',
+	function ($scope, Authentication) {
 
-		var ref = new Firebase(FIREBASE_URL);
-		var auth = $firebaseAuth(ref);
-
-		// Login
 		$scope.login = function(){
-			$scope.message = "Welcome " + $scope.user.email;
-		};
+			Authentication.login($scope.user);
+		}; // close Login from Authentication
 
-		// register
 		$scope.register = function(){
-			// createUser
-			auth.$createUser({
-				email: $scope.user.email,
-				password: $scope.user.password
-			}).then(function (user) {
-				$scope.message = "Welcome " + $scope.user.fname + ", Thank you for registering.";
-			}).catch(function (error) {
-				$scope.message = error.message;
-			})
-		};
-	}]);
+			Authentication.register($scope.user);
+		}; // close register from Authentication
+
+	}]); // close Registration Controller function.
--- public/js/services/authentication.js
+++ public/js/services/authentication.js
@@ -1,31 +1,23 @@
 fobApp.factory('Authentication', ['$rootScope', '$firebaseAuth', 'FIREBASE_URL', function ($rootScope, $firebaseAuth, FIREBASE_URL) {
-	var ref = new Firebase(FIREBASE_URL);
-	var auth = $firebaseAuth(ref);
 
-	return{
-		login: function (user) {
-			$rootScope.message = "Welcome " + $scope.user.email;
-		}, // close login return
+		var ref = new Firebase(FIREBASE_URL);
+		var auth = $firebaseAuth(ref);
 
-		register: function(user) {
-			auth.$createUser({
-				email: user.email,
-				password: user.password
-			}).then(function (user) {
-				$rootScope.message = "Welcome " + user.fname + ", Thank you for registering.";
-			}).catch(function (error) {
-				$rootScope.message = error.message;
-			}); // close createUser auth function
-		} // close register return
-	}; // close return function
+		return{
+			login: function (user) {
+				$rootScope.message = "Welcome " + $scope.user.email;
+			}, // close login return
 
-}]); // close factory
+			register: function(user) {
+				auth.$createUser({
+					email: user.email,
+					password: user.password
+				}).then(function (rUser) {
+					$rootScope.message = "Hello " + user.fname + ", Thank you for registering.";
+				}).catch(function (error) {
+					$rootScope.message = error.message;
+				}); // close createUser auth function
+			} // close register return
+		}; // close return function
 
-
-
-
-
-fobApp.controller('RegistrationController', [
-	'$scope',
-	'$firebaseAuth',
-	'FIREBASE_URL', function ($scope, $firebaseAuth, FIREBASE_URL) {
+	}]); // close factory
