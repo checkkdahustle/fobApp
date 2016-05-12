0a7ff94744d57b8fdaa717e0363ffe78359030cf
checkkdahustle <checkkdahustle@fullsail.edu>
2016-05-12 03:44:06 -0400

    added a missing dependencies in the controller.

--- public/js/controllers/registration.js
+++ public/js/controllers/registration.js
@@ -1,16 +1,26 @@
 fobApp.controller('RegistrationController', [
 	'$scope',
 	'$firebaseAuth',
-	'FIREBASE_URL', function ($scope, $firebase, FIREBASE_URL) {
+	'FIREBASE_URL', function ($scope, $firebaseAuth, FIREBASE_URL) {
 
 		var ref = new Firebase(FIREBASE_URL);
 		var auth = $firebaseAuth(ref);
 
+		// Login
 		$scope.login = function(){
 			$scope.message = "Welcome " + $scope.user.email;
 		};
 
+		// register
 		$scope.register = function(){
-			$scope.message = "Welcome " + $scope.user.fname;
+			// createUser
+			auth.$createUser({
+				email: $scope.user.email,
+				password: $scope.user.password
+			}).then(function (user) {
+				$scope.message = "Welcome " + $scope.user.fname + ", Thank you for registering.";
+			}).catch(function (error) {
+				$scope.message = error.message;
+			})
 		};
 	}]);
