e0bc05fafbf0009e515f8c17e060cfcb1c5c644e
checkkdahustle <checkkdahustle@fullsail.edu>
2016-05-14 19:34:57 -0400

    trying to modify gulpfile to server due to file changes.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 var gulp = require('gulp');
-
 var nodemon = require('gulp-nodemon');
+
 
 gulp.task('server', function () {
 	nodemon({
 		script: "server.js",
+		// watch: "public/",
 		ext: "js",
-		ignore: ["gulp*", "public*"]
-	})
-})
+		ignore: ["gulp*", "public*"],
+	}).on('restart', function() {
+		console.log('restarted');
+	});
+});
+
 
 gulp.task('default', ['server'], function () {
 		console.log('Server is ON');
