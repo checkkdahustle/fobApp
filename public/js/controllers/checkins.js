25045f524899b3fa5b3c1927c94a491191185a02
checkkdahustle <checkkdahustle@fullsail.edu>
2016-05-15 23:23:57 -0400

    add new controller to index, add checkin link to causes html, add a missing comma in the controller dep, and remove unused image link in css.

---
+++ public/js/controllers/checkins.js
@@ -0,0 +1,4 @@
+fobApp.controller('CheckinsController', ['$scope', '$rootScope', '$firebaseObject', '$firebaseArray', '$routeParams', 'FIREBASE_URL', function ($scope, $rootScope, $firebaseObject, $firebaseArray, $routeParams, FIREBASE_URL) {
+
+
+}]); // close CheckinsController.
