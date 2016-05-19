711f7a42f79effee5f1b1063948baf67fbc73f98
checkkdahustle <checkkdahustle@fullsail.edu>
2016-05-19 17:20:30 -0400

    change the causeRef path to create its own table in the database. Also added ownerName and ID to every new cause created.

--- public/js/controllers/causes.js
+++ public/js/controllers/causes.js
@@ -3,9 +3,43 @@ fobApp.controller('CausesController', ['$scope', '$rootScope', '$firebaseAuth', 
 	var ref = new Firebase(FIREBASE_URL);
 	var auth = $firebaseAuth(ref);
 
+/*
+
+users path - 'firebase/users/' + authData.uid
+users causes path - 'firebase/users/' + authData.uid + '/cause'
+^^^ this is a collection of firebase ids when a cause is created
+cause path - 'firebase/cause/' <== this has a firebase ID
+cause owner ID - 'firebase/cause/' + firebasedata.key + '/owner'
+
+*/
+
+/*
+
+
+$routeProvider.when('/users/:userID', {
+
+	templateUrl: 'yourView.html',
+	controller: yourCtrl
+})
+
+in yourCtrl >
+$routeParams into the controller($scope, etc, etc)
+
+firebaseURL = blah/users/$routeParams.userID
+
+now you have his info
+
+
+*/
+
+
+
 	auth.$onAuth(function(authUser) {
 		if (authUser) {
-			var causesRef = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/causes');
+			// var causesRef = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/causes');
+			//console.log('auth info', authUser)
+			//console.log('rootscope stuff', $scope.currentUser)
+			var causesRef = new Firebase(FIREBASE_URL + '/causes');
 			var causesInfo = $firebaseArray(causesRef);
 			$scope.causes = causesInfo;
 
@@ -21,9 +55,21 @@ fobApp.controller('CausesController', ['$scope', '$rootScope', '$firebaseAuth', 
 			$scope.addCause = function() {
 				causesInfo.$add({
 					name: $scope.causeName,
+					ownerName:  $scope.currentUser.firstName + " " +  $scope.currentUser.lastName,
+					ownerID:  $scope.currentUser.$id,
 					date: Firebase.ServerValue.TIMESTAMP
-				}).then(function() {
-					$scope.causeName='';
+				})
+				.then(function(ref) {
+					var key = ref.key();
+					console.log("Added: ", key);
+					$scope.currentUser.causes[key] = $scope.causeName;
+					$scope.currentUser.$save().then(function(ref) {
+					  key === $scope.currentUser.$id; // true
+						$scope.causeName='';
+						console.log("it works!")
+					}, function(error) {
+					  console.log("Error:", error);
+					}); // error
 				}); // close Promise
 			}; // close 'Add Cause' function.
 
